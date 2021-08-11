c5e3e05a6f750257034e988700726c4a20487333
jihye <asd36384@gmail.com>
2021-08-11 19:06:06 +0900

    cardview

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Header from '../Components/Header';
  
 const MainPage = () => {
@@ -8,12 +7,3 @@ const MainPage = () => {
   );
 };
 export default MainPage;
-=======
-import CardView from "../Components/CardView";
-
-const MainPage = () => {
-  return <CardView />;
-};
-
-export default MainPage;
->>>>>>> 09183584d637438073de210e4aa3eb6d335f2d46

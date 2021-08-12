e3212931a6bf31dc6dae5dd11cc9c15fced8f944
abi-hong <flffjakfwm20@gmail.com>
2021-08-12 21:11:01 +0900

    add heart icon & adding heart icon click

--- src/Components/CardView.js
+++ src/Components/CardView.js
@@ -1,38 +1,78 @@
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class CardView extends React.Component {
-  render() {
-    return (
-      <Card style={{ width: "18rem", position: "relative" }}>
-        <Card.Link href="#first">
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>파이썬 공부</Card.Title>
-            <Card.Text>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>분류: 스터디</ListGroupItem>
-                <ListGroupItem>기간: 2020/07/07 ~ 2020/09/07</ListGroupItem>
-                <ListGroupItem>총 인원: 5</ListGroupItem>
-                <ListGroupItem>
-                  이런 분 원해요!
-                  <ul style={{ marginTop: "10px" }}>
-                    <li>파이썬이 주 언어이신 분</li>
-                    <li>머신 러닝에 관심이 많으신 분</li>
-                  </ul>
-                </ListGroupItem>
-              </ListGroup>
-            </Card.Text>
-            <Card.Img
-              style={{ position: "absolute", bottom: "10px", right: "10px" }}
-              variant="bottom"
-              src="/Users/abi/Desktop/snowcode_front/public/fillheart.png/40px40"
-            />
-          </Card.Body>
-        </Card.Link>
-      </Card>
-    );
-  }
+function CardView() {
+  const [currentClick, setCurrentClick] = useState(null);
+  const [prevClick, setPrevClick] = useState(null);
+
+  const GetClick = (e) => {
+    setCurrentClick(e.target.id);
+    setPrevClick(e.target.id);
+  };
+
+  useEffect(
+    (e) => {
+      if (currentClick !== null) {
+        let current = document.getElementById(currentClick);
+        console.log(current);
+        current.style.img = "blankheart.png";
+      }
+      if (prevClick !== null) {
+        let prev = document.getElementById(prevClick);
+        prev.style.img = "fillheart.png";
+      }
+      setPrevClick(currentClick);
+    },
+    [currentClick]
+  );
+
+  return (
+    <Card
+      id="card"
+      style={{ width: "18rem", textDecoration: "none", position: "relative" }}
+    >
+      <Card.Link href="#first" style={{ textDecoration: "none" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>파이썬 공부</Card.Title>
+          <Card.Text>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>분류: 스터디</ListGroupItem>
+              <ListGroupItem>기간: 2020/07/07 ~ 2020/09/07</ListGroupItem>
+              <ListGroupItem>총 인원: 5</ListGroupItem>
+              <ListGroupItem>
+                이런 분 원해요!
+                <ul style={{ marginTop: "10px" }}>
+                  <li>파이썬이 주 언어이신 분</li>
+                  <li>머신 러닝에 관심이 많으신 분</li>
+                </ul>
+              </ListGroupItem>
+            </ListGroup>
+          </Card.Text>
+        </Card.Body>
+      </Card.Link>
+      <div
+        style={{
+          display: "block",
+          width: "40px",
+          height: "40px",
+          position: "absolute",
+          bottom: "10px",
+          right: "10px",
+        }}
+      >
+        <img
+          id="case"
+          name="heart"
+          width="40px"
+          height="40px"
+          src="fillheart.png"
+          alt="찜하기버튼"
+          onClick={GetClick}
+        ></img>
+      </div>
+    </Card>
+  );
 }
 
 export default CardView;

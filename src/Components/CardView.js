01df414e81b392da3075a0484d121a50e540d727
jihye <asd36384@gmail.com>
2021-08-18 16:39:03 +0900

    doing mainpage

--- src/Components/CardView.js
+++ src/Components/CardView.js
@@ -2,6 +2,45 @@ import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import React from "react";
 
 function CardView(props) {
+  const study = props.study;
+
+  const checkHeart = study.heart;
+  const startDate = new Date(study.startLine.toString());
+  const endDate = new Date(study.deadLine.toString());
+
+  const formatDate = (date)=>{
+    let formatted_date = date.getFullYear() + "년" + (date.getMonth() + 1) + "월" + date.getDate() + "일"
+    return formatted_date;
+  }
+
+  let heart;
+  if (checkHeart) {
+    heart = <img width='40px' height= '40px' src='fillheart.jpg' alt="찜하기버튼"></img>
+  } else {
+    heart = <img width='40px' height= '40px' src='blankheart.jpg' alt="찜하기버튼"></img>;
+  }
+
+  let cardViewImg;
+  if (props.study.category === "스터디") {
+    cardViewImg = <Card.Img
+            variant="top" 
+            alt=""
+            src={require("../Assets/스터디.png").default}
+            />
+  } else if (props.study.category === "공모전") {
+    cardViewImg = <Card.Img
+            variant="top" 
+            alt=""
+            src={require("../Assets/공모전.png").default}
+            />
+  } else {
+    cardViewImg = <Card.Img
+            variant="top" 
+            alt=""
+            src={require("../Assets/졸업작품.png").default}
+            />
+  }
+
   const onClick = () => {
     const getdisplay = this.props.getdisplay
     if(window.confirm('찜을 해제하시겠습니까? \n찜을 해제하면 목록에서 해당 모집글이 없어집니다'))
@@ -11,26 +50,22 @@ function CardView(props) {
     <Card id="card" style={{display:'block', width: '20rem'}}>
         <Card.Link href="#first" style={{textDecoration:'none', position:'relative',}}>
           <div>
-            <Card.Img
-            variant="top" 
-            alt=""
-            src={require("../Assets/python.jpg").default}
-            />
+            {cardViewImg}
           </div>
           <Card.Body>
-            <Card.Title>파이썬 공부</Card.Title>
+            <Card.Title>{study.title}</Card.Title>
             <Card.Text>
               <ListGroup className="list-group-flush">
-                <ListGroupItem>분류: 스터디</ListGroupItem>
-                <ListGroupItem>기간: 2020/07/07 ~ 2020/09/07</ListGroupItem>
-                <ListGroupItem>총 인원: 5</ListGroupItem>
+                <ListGroupItem>분류: {study.category}</ListGroupItem>
+                <ListGroupItem>기간: {formatDate(startDate) + "~" + formatDate(endDate)}</ListGroupItem>
+                <ListGroupItem>총 인원: {study.people}</ListGroupItem>
               </ListGroup>
             </Card.Text>
           </Card.Body>
         </Card.Link>
-        <div style={{display: 'block', width: '40px', height: '40px', position:'absolute',bottom: '10px', right: '10px'}}
+        <div style={{display: 'block', position:'absolute',bottom: '10px', right: '15px'}}
           onClick={onClick}>
-          <img width='40px' height= '40px' src='fillheart.jpg' alt="찜하기버튼"></img>
+          {heart}
         </div>
       </Card>
   );
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from '../Api/api'
 import Header from '../Components/Header';
 import CardView from '../Components/CardView';
 import { Row, Col, Card } from "react-bootstrap";
  
-class MainPage extends React.Component {
-  
-  render() {
-    const search = this.props.location.search;
-    const params = new URLSearchParams(search);
-    const idFromUrl = params.get('id');
-    return (
-      <div>
-        <Header id={idFromUrl}/>
-        <Row 
-          xs={1}
-          md={5}
-          className="g-6"
-          style={{marginLeft: "50px", marginTop: "50px", marginRight: "50px", marginBottom: "50px"}}
-          >
-          {Array.from({ length: 10 }).map((_, idx) => (
+function MainPage(props) {
+  //유저 fetch
+  const search = props.location.search;
+  const params = new URLSearchParams(search);
+  const idFromUrl = params.get('id');
+
+  //study fetch
+  const [study, setStudy] = useState([]);
+
+  useEffect(() => {
+      const fetchStudy = async () => {
+          try {
+              const response = await api.get(
+              `/study`
+              );
+              setStudy(response.data); // 데이터는 response.data 안에 들어있습니다.
+          } catch (e) {
+          }
+      };
+      fetchStudy();
+  }, []);
+  return (
+    <div>
+      <Header id={idFromUrl}/>
+      <Row 
+        xs={1}
+        md={5}
+        className="g-6"
+        style={{marginLeft: "50px", marginTop: "50px", marginRight: "50px", marginBottom: "50px"}}
+        >
+        {study.map((study) => {
+          return (
             <Col style={{marginBottom: "25px"}}>
-              <CardView/>
+              <CardView study={study}/>
             </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
-};
+          )})
+        }
+      </Row>
+    </div>
+  )
+}
 export default MainPage;
